1990cb2e7b42a9fcb3bb00eaf0b75d90d9966151
emilio alvarado <emiliolvrd@gmail.com>
2017-04-10 15:14:32 -0500

    finished first part of the switch exercise

--- js/switch.js
+++ js/switch.js
@@ -1,4 +1,31 @@
 /**
  * Created by emilioalvarado on 4/10/17.
  */
-var luckyNumber = Math.floor(Math.random()* 6)
+"use strict";
+var luckyNumber = Math.floor(Math.random() * 6);
+
+console.log(luckyNumber);
+
+var receipt = 60;
+
+switch (luckyNumber) {
+    case 0:
+        console.log(receipt);
+        break;
+    case 1:
+        console.log(receipt - (receipt * .10));
+        break;
+    case 2:
+        console.log(receipt - (receipt * .25));
+        break;
+    case 3:
+        console.log(receipt - (receipt * .50));
+        break;
+    case 4:
+        console.log(receipt - (receipt * .75));
+        break;
+    case 5:
+        console.log(receipt * 0,' its free');
+    default:
+        console.log('Nothing happened.')
+}
