0e4e55a80bae646bc70173cd076cf8a2eb2c02af
emilio alvarado <emiliolvrd@gmail.com>
2017-04-10 11:49:20 -0500

    finished if else exercises

---
+++ js/external.js
@@ -0,0 +1,5 @@
+/**
+ * Created by emilioalvarado on 4/10/17.
+ */
+"use strict"
+console.log('Hello from external JavaScript!');
---
+++ js/if-else.js
@@ -0,0 +1,41 @@
+/**
+ * Created by emilioalvarado on 4/10/17.
+ */
+var gradeAverage = (70 + 80 + 95) / 3;
+if (gradeAverage > 80) {
+    console.log('Youre awesome')
+} else {
+    console.log('You need more practice.')
+}
+
+
+var ryan = 250
+var cameron = 180
+var george = 320
+if (ryan > 200) {
+    console.log('Ryan bought more than 200 dollars worth of product. Ryans final payment is ' + ryan * .35)
+} else {
+    console.log('Ryan did not buy enough product. Ryans final payment is' + ryan)
+}
+
+if (cameron > 200) {
+    console.log('Cameron bought more than 200 dollars worth of product. Ryans final payment is ' + cameron * .35)
+} else {
+    console.log('Cameron did not buy enough product. Ryans final payment is ' + cameron)
+}
+
+if (george > 200) {
+    console.log('George bought more than 200 dollars worth of product. Ryans final payment is ' + george * .35)
+} else {
+    console.log('George did not buy enough product. Ryans final payment is' + george)
+}
+
+
+var flipACoin = Math.floor(Math.random() * 2);
+console.log('flipACoin' + flipACoin)
+if (flipACoin == 0) {
+    console.log('buy car');
+} else {
+    console.log('buy house');
+}
+
