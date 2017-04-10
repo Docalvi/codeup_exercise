5e0baac160f516430e919c1259d546555cbb2704
emilio alvarado <emiliolvrd@gmail.com>
2017-04-10 12:18:14 -0500

    fixed and finished second exercise

--- js/if-else.js
+++ js/if-else.js
@@ -13,26 +13,30 @@ var ryan = 250
 var cameron = 180
 var george = 320
 if (ryan > 200) {
-    console.log('Ryan bought more than 200 dollars worth of product. Ryan\'s final payment is ' + ryan * .35)
+    console.log('Ryan paid '+
+    ryan, ' dollars, he bought more than 200 dollars worth of product. Ryan\'s final payment is ' + ryan * .35
+)
 } else {
     console.log('Ryan did not buy enough product. Ryan\'s final payment is ' + ryan)
 }
 
 if (cameron > 200) {
-    console.log('Cameron bought more than 200 dollars worth of product. Cameron\'s final payment is ' + cameron * .35)
+    console.log('Cameron paid '+
+        cameron, ' dollars, he bought more than 200 dollars worth of product. Cameron\'s final payment is ' + cameron * .35)
 } else {
     console.log('Cameron did not buy enough product. Cameron\'s final payment is ' + cameron)
 }
 
 if (george > 200) {
-    console.log('George bought more than 200 dollars worth of product. George\'s final payment is ' + george * .35)
+    console.log('George paid '+
+        george, ' dollars, he bought more than 200 dollars worth of product. George\'s final payment is ' + george * .35)
 } else {
     console.log('George did not buy enough product. George\'s final payment is ' + george)
 }
 
 
 var flipACoin = Math.floor(Math.random() * 2);
-console.log('flipACoin' + flipACoin)
+console.log('flipACoin = ' + flipACoin)
 if (flipACoin == 0) {
     console.log('buy car');
 } else {
