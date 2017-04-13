d976668737e7b71fab8264f8f02fe67f292dc723
emilio alvarado <emiliolvrd@gmail.com>
2017-04-13 08:35:42 -0500

    finished iterating exercise

--- js/iterating.js
+++ js/iterating.js
@@ -4,10 +4,22 @@
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
 var name = ['Josh', 'Gus', 'Jorge', 'Ashley'];
     // TODO: Create a log statement that will log the number of elements in the names array.
-console.log(name.length + 'elements');
+console.log(name.length + ' elements');
     // TODO: Create log statements that will print each of the names array elements individually.
     console.log('First name is ' + name[0]);
     console.log('Second name is ' + name[1]);
     console.log('Third name is ' + name[2]);
     console.log('Fourth name is ' + name[3]);
+
+
+
+    for (var i = 0; i < name.length; i++) {
+        console.log('The name at index ' + i + ' is: ' + name[i]);
+    }
+
+name.forEach(function (element, index, array){
+    console.log(element)
+});
+
+
 })();
